2881df851f8f1140cfded897f20131876d051459
Sarahsalami <ssalam06@bbk.ac.uk>
2026-08-11 15:21:47 +0100

    Protect authentication routes with JWT middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
@@ -120,5 +121,11 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.get("/me", protect, async (req, res) => {
+  res.status(200).json({
+    success: true,
+    user: req.user,
+  });
+});
 
 module.exports = router;
